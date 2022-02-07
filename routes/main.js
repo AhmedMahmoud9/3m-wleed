22b734604b42f2c6f374dbfe523663c4331f620a
Ahmed Mahmoud <wtt.wtt13@gmail.com>
2022-02-07 12:41:50 +0200

    first

---
+++ routes/main.js
@@ -0,0 +1,30 @@
+// Full Documentation - https://docs.turbo360.co
+const express = require('express')
+const router = express.Router()
+
+/*  This is the home route. It renders the index.mustache page from the views directory.
+  Data is rendered using the Mustache templating engine. For more
+  information, view here: https://mustache.github.io/#demo */
+router.get('/', (req, res ,next) => {
+    const data = req.context
+  res.render("home",data)
+})
+
+router.get('/market', (req, res ,next) => {
+    const data = req.context
+  res.render("market",data)
+})
+
+router.get('/portflio', (req, res ,next) => {
+    const data = req.context
+  res.render("portflio",data)
+})
+
+router.get('/contact', (req, res ,next) => {
+    const data = req.context
+  res.render("contact",data)
+})
+
+
+
+module.exports = router
